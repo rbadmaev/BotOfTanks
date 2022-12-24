75f5071743ad94d14e3486e5dfb9bd727bd77bfd
Roman Badmaev <barv@inbox.ru>
2022-12-24 08:26:29 +0300

    Print found bots

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.8
+
 from Game import Game
 from Vector import Vector
 
@@ -5,6 +7,7 @@ import os
 
 game = Game(Vector(x=800, y=600))
 for path in os.listdir("bots/"):
+    print("Found bot ", path)
     game.addBot(os.path.abspath(os.path.join("bots", path)))
 
 game.start()
